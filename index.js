1f6e19ec1c9d3579c11b06c90bd10a4a93143898
aitaelf <aitaelf@mail.ru>
2017-06-02 02:53:19 +0300

    p2

--- index.js
+++ index.js
@@ -1,8 +1,37 @@
 const fs = require('fs');
 const input = fs.createReadStream('data.txt');
 const output = fs.createWriteStream('result.txt');
+const transform = require('stream').Transform;
 const crypto = require('crypto');
 const hash = crypto.createHash('md5');
+const hash2 = crypto.createHash('md5');
 
-input.pipe(hash).pipe(process.stdout); // в консоль
-input.pipe(hash).pipe(output); // в файл
+class CTransform extends transform {
+	constructor(options){
+		super(options);
+	}
+	_transform(chunk, encoding, done) {
+		hash2.update(chunk);
+		done(null);
+	}
+	_flush(done) {
+		let result = hash2.digest('hex');
+		done(null, result);
+	}
+}
+
+const t1 = new CTransform();
+
+input
+	.pipe(t1)
+	.pipe(output);
+input
+	.pipe(t1)
+	.pipe(process.stdout);
+
+input
+	.pipe(hash)
+	.pipe(output);
+input
+	.pipe(hash)
+	.pipe(process.stdout);
